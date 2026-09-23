1ec82fec2b1a74cf3fdf8baa60ddb855cb9cb132
m1drage <m1drage@mail.ru>
2026-09-23 20:55:27 +0500

    Добавила создание getElement с подменой числа в индексе

--- lab1.3.cpp
+++ lab1.3.cpp
@@ -13,10 +13,21 @@ SafeArray createArray(int  size) {
     return arr;
 }
 
+int& getElement(SafeArray& arr, int index) {
+    if(index < 0 || index >= arr.size) {
+        cout << "Error" << endl;
+        static int error = 0;
+        return error;
+    }
+    return arr.data[index];
+}
+
 int main() {
     SafeArray myArr = createArray(5);
     cout << myArr.size << endl;
 
+    getElement(myArr, 7) = 999;
+    cout << getElement(myArr, 2) << endl;
 
     return 0;
 }
