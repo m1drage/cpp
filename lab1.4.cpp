9181951bf8c06d3192b2c88bae83f2bc4f96eb9f
m1drage <m1drage@mail.ru>
2026-09-23 23:35:41 +0500

    allocateMatrix для выделения памяти и fillMatrix для заполнения

---
+++ lab1.4.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int** allocateMatrix(int rows, int cols) {
+    int** matrix = new int*[rows];
+
+    for(int i = 0; i < rows; i++) {
+        matrix[i] = new int[cols];
+    }
+    return matrix;
+}
+
+void fillMatrix(int** matrix, int rows, int cols) {
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < cols; j++) {
+            cin >> matrix[i][j];
+        }
+    }
+}
+
+int main() {
+
+}
